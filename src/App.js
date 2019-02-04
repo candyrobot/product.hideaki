1596ea173f5fa67331d3f95c8f38c2ffb3618b6f
田中 秀明 <tanaka_hideaki@aja-kk.co.jp>
2019-02-04 16:37:47 +0900

    create

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// INFO: これが何なのかは秀明にきいてねw
+import Hideaki from './Hideaki';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Hideaki />
+      </div>
+    );
+  }
+}
+
+export default App;
